ac64b26131e1f4786a258cda5351558ed061c949
Cameron Pittman <cameron.w.pittman@gmail.com>
2021-12-01 15:33:18 -0500

    add day 1 part 2

--- day1.lisp
+++ day1.lisp
@@ -13,3 +13,35 @@
    append (list (parse-integer depth)) into history
    finally
       (return total)))
+
+;; part 2
+(defparameter *test-nums* '(
+                            "199"
+                            "200"
+                            "208"
+                            "210"
+                            "200"
+                            "207"
+                            "240"
+                            "269"
+                            "260"
+                            "263"
+                            ))
+
+(defun sum (nums)
+  "Get the sum of a list of numbers"
+  (reduce #'+ nums))
+
+(print
+ (loop
+   for depth in (uiop:read-file-lines "day1-data.txt")
+   ;; for depth in *test-nums*
+   if (and
+       (not (null history))
+       (>= (list-length history) 3)
+       (> (sum (append (list (parse-integer depth)) (last history 2)))
+          (sum (last history 3))))
+     count depth into total
+   append (list (parse-integer depth)) into history
+   finally
+      (return total)))

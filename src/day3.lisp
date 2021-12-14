263efaae4a3d95a5d746067e53fc61ab31a9093b
Cameron Pittman <cameron.w.pittman@gmail.com>
2021-12-14 12:47:46 -0500

    add day 3 part 2

--- src/day3.lisp
+++ src/day3.lisp
@@ -44,3 +44,101 @@
     (bits->int (map 'bit-vector
                     #'(lambda (b) (if (< b 500) 1 0))
                     summary)))))
+
+;; part 2
+
+(defun str->list (input)
+  "Convert an string of numbers into a list of its corresponding digits"
+  ;; TODO: there's probably a better way of doing this
+  (map 'list #'digit-char-p (coerce input 'list)))
+
+(defun most-common-value (sum num)
+  "Most common value"
+  (if (>= sum (/ num 2)) 1 0))
+
+(defun least-common-value (sum num)
+  "Least common value"
+  (if (= (most-common-value sum num) 1) 0 1))
+
+(defun filter-o2 (n o2-lines o2-mcv)
+  "Perform O2 algo by filtering the list of inputs based on MCV. Returns new list and new MCV"
+  (loop for iline in o2-lines
+        while (> (list-length o2-lines) 1)
+        if (= (nth n iline) o2-mcv)
+          ;; get the next set of lines ready
+          append (list iline) into filtered-o2-lines and
+        ;; update the next position if we haven't reached the end of the list
+        if (< (1+ n) (list-length iline))
+          sum (nth (1+ n) iline) into o2-mcv-next
+        finally (return (values filtered-o2-lines (most-common-value o2-mcv-next (list-length filtered-o2-lines))))))
+
+(defun filter-co2 (n co2-lines co2-lcv)
+  "Perform CO2 algo by filtering the list of inputs based on LCV. Returns new list and new LCV"
+  (if (= (list-length co2-lines) 1) co2-lines
+      (loop for iline in co2-lines
+            if (= (nth n iline) co2-lcv)
+              ;; get the next set of lines ready
+              append (list iline) into filtered-co2-lines and
+            ;; update the next position if we haven't reached the end of the list
+            if (< (1+ n) (list-length iline))
+              sum (nth (1+ n) iline) into co2-lcv-next
+            finally (return (values filtered-co2-lines (least-common-value co2-lcv-next (list-length filtered-co2-lines)))))))
+
+(defun filter-down-ratings (lines)
+  (multiple-value-bind (lines-init o2-mcv-first)
+      (loop for input in lines
+            for iline = (str->list input)
+            sum (car iline) into mcvs
+            append (list iline) into ilines
+            finally (return (values ilines (most-common-value mcvs (list-length ilines)))))
+    (let* ((o2-lines (copy-list lines-init))
+           (co2-lines (copy-list lines-init))
+           (o2-mcv o2-mcv-first)
+           ;; opposite of O2 - least common value
+           (co2-lcv (if (= o2-mcv 1) 0 1)))
+
+      ;; loop over all the bits
+      (loop for n upto 11
+            do (progn
+                 ;; filter for the O2 generation rating
+                 (multiple-value-bind (new-o2-lines new-o2-mcv)
+                    (filter-o2 n o2-lines o2-mcv)
+                   (setf o2-lines (copy-list new-o2-lines))
+                   (setf o2-mcv new-o2-mcv))
+
+                 ;; filter for the CO2 scrubbing rating
+                 (multiple-value-bind (new-co2-lines new-co2-lcv)
+                    (filter-co2 n co2-lines co2-lcv)
+                   (setf co2-lines (copy-list new-co2-lines))
+                   (setf co2-lcv new-co2-lcv)))
+            finally (return (values o2-lines co2-lines))))))
+
+(defun day3part2 ()
+  "Day 3 part 2"
+ (let* ((report (read-puzzle-input-lines "day3.txt")))
+   (multiple-value-bind (o2-lines co2-lines)
+       (filter-down-ratings report)
+     (*
+      (bits->int (car o2-lines))
+      (bits->int (car co2-lines))))))
+
+(print (day3part2))
+
+(day3part2)
+
+;; (defun day3part2 ()
+  ;; "Day 3 part 2"
+ ;; (let* ((report (read-puzzle-input-lines "day3.txt"))
+        ;; a list of 1s and 0s representing the most common digit at each position in the list of inputs
+        ;; (summary (loop for input in report
+                       ;; for digits = (map 'list #'digit-char-p (coerce input 'list))
+                       ;; with summary = '(0 0 0 0 0 0 0 0 0 0 0 0)
+                       ;; do (setf summary (mapcar #'+ summary digits))
+                       ;; finally (return (map 'integer #'(lambda (b) (if (>= b 500) 1 0)) summary)))))
+   ;; (dotimes (n (list-length summary))
+               ;; (print (nth n summary)))
+   ;; (*
+    ;; O2 generation rating
+    ;; (loop )
+    ;; CO2 scrubbing rating
+    ;; ()))
